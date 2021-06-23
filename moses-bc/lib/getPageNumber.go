219a10d57752a57b7f2ca3b50f7857baeb29f919
Paul Matencio <paulo.matencio@gmail.com>
2021-06-23 20:18:49 +0200

    add check backup

--- moses-bc/lib/getPageNumber.go
+++ moses-bc/lib/getPageNumber.go
@@ -2,7 +2,6 @@ package lib
 
 import (
 	"errors"
-	base64 "github.com/paulmatencio/ring/user/base64j"
 	"github.com/paulmatencio/s3c/gLog"
 	moses "github.com/paulmatencio/s3c/moses/lib"
 	sproxyd "github.com/paulmatencio/s3c/sproxyd/lib"
@@ -19,7 +18,7 @@ func GetPageNumber(key string) (int, error, int) {
 				Timeout:   sproxyd.ReadTimeout,
 				Transport: sproxyd.Transport,
 			},
-			Path: sproxyd.Env + "/" + key ,
+			Path: sproxyd.Env + "/" + key,
 		}
 		resp *http.Response
 	)
@@ -30,19 +29,15 @@ func GetPageNumber(key string) (int, error, int) {
 		switch resp.StatusCode {
 		case 200:
 			encoded_usermd := resp.Header["X-Scal-Usermd"]
-			if usermd, err := base64.Decode64(encoded_usermd[0]); err == nil {
-				gLog.Info.Println(string(usermd))
-				if err = metadata.UsermdToStruct(string(usermd)); err == nil {
-
-					if pgn, err = metadata.GetPageNumber(); err == nil {
-						gLog.Info.Printf("Key %s  - Number of pages %d",key,pgn)
-						return pgn, err, resp.StatusCode
-					} else {
-						gLog.Error.Printf("Error getting page number %d",pgn)
-					}
-				}else {
-					gLog.Error.Println(err)
+			if err = metadata.UsermdToStruct(string(encoded_usermd[0])); err == nil {
+				if pgn, err = metadata.GetPageNumber(); err == nil {
+					gLog.Info.Printf("Key %s  - Number of pages %d", key, pgn)
+					return pgn, err, resp.StatusCode
+				} else {
+					gLog.Error.Printf("Error getting page number %d", pgn)
 				}
+			} else {
+				gLog.Error.Println(err)
 			}
 		default:
 			err = errors.New("Check the status Code for the reason")
